7a63ebefc625cb2c163c5154774a257905b515dc
nabilhudarizalulhaq <nabilhuda81@gmail.com>
2023-08-04 22:50:59 +0700

    slicing top up succes

--- lib/ui/screen/topup_amount_page.dart
+++ lib/ui/screen/topup_amount_page.dart
@@ -1,4 +1,6 @@
 import 'package:bank_sha/shared/theme.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:intl/intl.dart';
 import 'package:bank_sha/ui/widgets/buttons.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +14,26 @@ class TopupAmountPage extends StatefulWidget {
 class _TopupAmountPageState extends State<TopupAmountPage> {
   final TextEditingController amountController =
       TextEditingController(text: '0');
+
+  @override
+  void initState() {
+    super.initState();
+
+    amountController.addListener(() {
+      final text = amountController.text;
+
+      amountController.value = amountController.value.copyWith(
+        text: NumberFormat.currency(
+          locale: 'id',
+          decimalDigits: 0,
+          symbol: '',
+        ).format(int.parse(
+          text.replaceAll('.', ''),
+          ),
+        ),
+      );
+    });
+  }
 
   addAmount(String number) {
     if (amountController.text == '0') {
@@ -190,6 +212,8 @@ class _TopupAmountPageState extends State<TopupAmountPage> {
             title: 'Checkout Now',
             onPressed: () async {
               if (await Navigator.pushNamed(context, '/pin') == true) {
+                await launch('https://demo.midtrans.com/');
+
                 Navigator.pushNamedAndRemoveUntil(
                     context, '/topup-succes', (route) => false);
               }
@@ -200,7 +224,7 @@ class _TopupAmountPageState extends State<TopupAmountPage> {
           ),
           CustomTextButton(
             title: 'Terms & Conditions',
-            onPressed: (){},
+            onPressed: () {},
           ),
           const SizedBox(
             height: 40,
